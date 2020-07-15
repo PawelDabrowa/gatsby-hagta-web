3bdaccac02ad027686fe58bb70b9d02ecc9c56dc
Pawel Dabrowa <fox@Pawels-iMac.local>
2020-07-15 09:27:50 +0100

    image optimaze

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
+  pathPrefix: '/gatsby-hagta-web',
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hagta`,
+    description: `Website Hagta`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
